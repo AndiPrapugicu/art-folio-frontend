086db9fd9f5a1d3feb5dc81b1115f6378bdfb8b6
Andi Prapugicu <prapugicu.andi02@yahoo.com>
2025-05-21 11:52:59 +0300

    api changes

--- src/apiService.js
+++ src/apiService.js
@@ -17,27 +17,26 @@ export const addArtwork = async (artwork) => {
 };
 
 export const getArtworks = async () => {
-  const response = await axios.get(`${API_URL}artworks`);
+  const response = await axios.get(`${API_URL}/artworks`);
   return response.data;
 };
 
 export const createArtwork = async (artwork) => {
-  const response = await axios.post(`${API_URL}artworks`, artwork);
+  const response = await axios.post(`${API_URL}/artworks`, artwork);
   return response.data;
 };
 
 export const updateArtwork = async (id, artwork) => {
-  const response = await axios.put(`${API_URL}artworks/${id}`, artwork);
+  const response = await axios.put(`${API_URL}/artworks/${id}`, artwork);
   return response.data;
 };
 
 export const deleteArtwork = async (id) => {
-  await axios.delete(`${API_URL}artworks/${id}`);
+  await axios.delete(`${API_URL}/artworks/${id}`);
 };
 
-// Pentru încărcarea de fișiere, folosiți ruta 'upload'
 export const uploadArtwork = async (formData) => {
-  const response = await axios.post(`${API_URL}artworks/upload`, formData, {
+  const response = await axios.post(`${API_URL}/artworks/upload`, formData, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
